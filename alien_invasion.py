75d9e5e3cb3619b72e5eb09d8b9e7ecce9a405fa
Krzysztof Jacek Wlodarczyk <k.wlodarczyk@f5.com>
2022-02-10 14:14:15 -0800

    add _update_screen method

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ import pygame
 from settings import Settings
 from ship import Ship
 
+
 class AlienInvasion():
     """Overall class to manage game assets and behavior."""
 
@@ -13,7 +14,6 @@ class AlienInvasion():
 
         pygame.init()
         self.settings = Settings()
-
 
         self.screen = pygame.display.set_mode((self.settings.screen_width,
                                                self.settings.screen_height))
@@ -26,18 +26,20 @@ class AlienInvasion():
             if event.type == pygame.QUIT:
                 sys.exit()
 
+    def _update_screen(self):
+        self.screen.fill(self.settings.bg_color)
+
+        self.ship.blitme()
+
+        # Make the most recently drawn screen visible.
+        pygame.display.flip()
+
     def run_game(self):
         """Start the main loop for the game."""
         while True:
             # Watch for keyboard and mouse events.
             self._check_events()
-
-            self.screen.fill(self.settings.bg_color)
-
-            self.ship.blitme()
-
-            # Make the most recently drawn screen visible.
-            pygame.display.flip()
+            self._update_screen()
 
 
 if __name__ == '__main__':
